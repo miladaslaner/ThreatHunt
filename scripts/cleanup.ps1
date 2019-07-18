a633446f9de9f3190159ce4d9c99d514d69759cd
Milad Aslaner <51508845+MiladMSFT@users.noreply.github.com>
2019-07-18 10:41:52 +0900

    Update cleanup.ps1

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -56,8 +56,12 @@ Set-NetFirewallProfile -Enabled $OldFirewall
 Write-Host "Set Attack Surface Reduction rules into Audit Mode"
 Add-MpPreference -AttackSurfaceReductionRules_Ids D4F940AB-401B-4EFC-AADC-AD5F3C50688A, 5BEB7EFE-FD9A-4556-801D-275E5FFC04CC, 92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B, 3B576869-A4EC-4529-8536-B80A7769E899, 75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84, D3E037E1-3EB8-44C8-A917-57927947596D, BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550, 01443614-cd74-433a-b99e-2ecdc07bfc25, c1db55ab-c21a-4637-bb3f-a12568109d35, 9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2, d1e49aac-8f56-4280-b9ba-993a6d77406c, b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4, 26190899-1602-49e8-8b27-eb1d0a1ce869, 7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c, e6db77e5-3df2-4cf1-b95a-636979351e5b -AttackSurfaceReductionRules_Actions AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode, AuditMode
 
-Write-Host "Disable option to do RDP without an authenticated user"
+Write-Host "Set SetUserAuthenticationRequired to 1"
+$Preferences = Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices
+if ($Preferences.UserAuthenticationRequired -eq 0)
+{
 (Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(1)
+}
 
 Write-Host "Delete previously created Windows service"
 Get-Service -Displayname "*Friendly Friend*" | ForEach-object{ cmd /c  sc delete $_.Name}
