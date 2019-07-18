ee98c3a006e4e766a65805cc9d95636f407ce623
Milad Aslaner <51508845+MiladMSFT@users.noreply.github.com>
2019-07-18 10:46:46 +0900

    Update network.ps1

--- scripts/network.ps1
+++ scripts/network.ps1
@@ -14,19 +14,35 @@ if ($Preferences.UserAuthenticationRequired -eq 1)
 }
 
 # C2C communication
-Write-Host "Some random C2C communication"
+Write-Host "Use curl.exe to reach twitterdocs.com"
 curl.exe --version twitterdocs.com
+
+Write-Host "Use curl.exe to reach msupdater.com"
 curl.exe --version msupdater.com
+
+Write-Host "Use Invoke-WebRequest to reach msupdater.com"
 Invoke-WebRequest -URI msupdater.com -UseBasicParsing
+
+Write-Host "Use Invoke-WebRequest to reach sydwl.cn"
 Invoke-WebRequest -URI sydwl.cn -UseBasicParsing
+
+Write-Host "Use Invoke-WebRequest to reach dwn.rundll32.ml"
 Invoke-WebRequest -URI dwn.rundll32.ml -UseBasicParsing
+
+Write-Host "Use Invoke-WebRequest to reach twitterdocs.com"
 Invoke-WebRequest -URI twitterdocs.com -UseBasicParsing
+
+Write-Host "Use Invoke-WebRequest to reach www.aybc.so"
 Invoke-WebRequest -URI www.aybc.so -UseBasicParsing
+
+Write-Host "Use Invoke-WebRequest to reach a.ssvs.space"
 Invoke-WebRequest -URI a.ssvs.space -UseBasicParsing
+
+Write-Host "Use Invoke-WebRequest to reach 118.24.150.172"
 Invoke-WebRequest -URI 118.24.150.172 -UseBasicParsing
 
 # Grab available endpoints in network based on nmap scan and do psexec
-Write-Host "Use nmap results to do some PSExec magic..."
+Write-Host "Use results of Nmap to leverahe PSExec"
 $nmaplog = (Get-Content -Path $ThreatBox\logs\nmap.log | Where-Object {$_ -like '*Discovered open port*'})
 $nmaplogip = ($nmaplog  |  Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value
 foreach ($nmapip in $nmaplogip) {
