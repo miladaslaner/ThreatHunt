50e174e41d8fc5ef9bc7d0e34656e885962595bc
Milad Aslaner <51508845+MiladMSFT@users.noreply.github.com>
2019-07-19 22:44:31 +0900

    Update network.ps1

--- scripts/network.ps1
+++ scripts/network.ps1
@@ -13,7 +13,23 @@ if ($Preferences.UserAuthenticationRequired -eq 1)
 (Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
 }
 
+# Grab available endpoints in network based on nmap scan and do psexec
+Write-Host "Use results of Nmap to leverage PSExec"
+$nmaplog = (Get-Content -Path $ThreatBox\logs\nmap.log | Where-Object {$_ -like '*Discovered open port*'})
+$nmaplogip = ($nmaplog  |  Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value
+foreach ($nmapip in $nmaplogip) {
+if (test-Connection -Cn $nmapip -quiet) {
+    cd $ThreatBox\tools\
+    psexec \\$nmapip -u $domainUser -p $DomainPass PowerShell ipconfig
+} else {
+    "$nmapip is not available"
+}
+}
+
 # C2C communication
+$reply = Read-Host -Prompt "Warning this step use curl.exe and Invoke-WebRequest to known bad IP/domains. Only proceed If you absolutely need/want."
+if ( $reply -match "[yY]" ) { 
+
 Write-Host "Use curl.exe to reach twitterdocs.com"
 curl.exe --version twitterdocs.com
 
@@ -40,16 +56,4 @@ Invoke-WebRequest -URI a.ssvs.space -UseBasicParsing
 
 Write-Host "Use Invoke-WebRequest to reach 118.24.150.172"
 Invoke-WebRequest -URI 118.24.150.172 -UseBasicParsing
-
-# Grab available endpoints in network based on nmap scan and do psexec
-Write-Host "Use results of Nmap to leverage PSExec"
-$nmaplog = (Get-Content -Path $ThreatBox\logs\nmap.log | Where-Object {$_ -like '*Discovered open port*'})
-$nmaplogip = ($nmaplog  |  Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value
-foreach ($nmapip in $nmaplogip) {
-if (test-Connection -Cn $nmapip -quiet) {
-    cd $ThreatBox\tools\
-    psexec \\$nmapip -u $domainUser -p $DomainPass PowerShell ipconfig
-} else {
-    "$nmapip is not available"
-}
 }
